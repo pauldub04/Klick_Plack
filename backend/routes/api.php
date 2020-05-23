bdbdc389cc4cb5ddc411c14e7957d82b7a3e164c
pauldub04 <paul.dubnyakov#gmail.com>
2020-05-23 15:09:27 +0300

    added sanctum to backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,18 +2,40 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+use \App\User;
+
+
+Route::post('/get_token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required'
+    ]);
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+        /*return response([
+            'message' => ['The provided credentials are incorrect.']
+        ], 404);*/
+    }
+    return $user->createToken($request->device_name)->plainTextToken;
+
+    /*$token = $user->createToken($request->device_name)->plainTextToken;
+    $response = [
+        'role' => $user->role,
+        'token' => $token,
+        'email' => $user->email,
+    ];
+    return response($response, 201);*/
+});
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+Route::middleware('auth:sanctum')->get('/book/all', 'BookController@all');
+Route::middleware('auth:sanctum')->post('/book/add', 'BookController@add');
+Route::middleware('auth:sanctum')->get('/book/delete/{id}', 'BookController@delete');
+Route::middleware('auth:sanctum')->get('/book/change_availability/{id}', 'BookController@changeAvailability');
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
